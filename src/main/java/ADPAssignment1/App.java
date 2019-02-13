18b39bca45efa6d89fd26492e2042fb7ba95728c
Lance <lancejohnston3@gmail.com>
2019-02-13 13:43:48 +0200

    initial commit

---
+++ src/main/java/ADPAssignment1/App.java
@@ -0,0 +1,13 @@
+package ADPAssignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World how are you!" );
+    }
+}
---
+++ src/test/java/ADPAssignment1/AppTest.java
@@ -0,0 +1,20 @@
+package ADPAssignment1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
