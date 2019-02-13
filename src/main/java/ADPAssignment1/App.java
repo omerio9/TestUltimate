9aef3a545be3acbf1c030f49a39db099d8c131c4
omerio9 <omegambadu02@gmail.com>
2019-02-13 20:48:50 +0200

    I added one additional line from your code

--- src/main/java/ADPAssignment1/App.java
+++ src/main/java/ADPAssignment1/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World how are you today!" );
+        System.out.println( "I added the following line to the project!" );
     }
 }
